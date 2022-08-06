b9dfd748e9fa834b7127a245b814d57ea8f6d02c
Danial Amin <danialamin489@gmail.com>
2022-08-06 20:36:14 +0500

    added winning algorithm

--- script.js
+++ script.js
@@ -22,7 +22,89 @@ board.addEventListener("click", function (event) {
       ".container .mf .times"
     ).innerHTML;
     boxDiv.classList.add("addCross");
-  } else if (boxDiv.className === "" && circles <= crosses) {
+  } else if (boxDiv.className === "" && circles < crosses) {
     boxDiv.classList.add("addCircle");
   }
+
+  //
+
+  const boxDivs2 = document.querySelectorAll(".container .board div div");
+  const arr2 = [];
+  boxDivs.forEach(function (boxDiv) {
+    arr2.push(boxDiv.className);
+  });
+
+  const message = document.querySelector(".message");
+  const whoWon = message.querySelector(".message_container p span");
+
+  if (arr2[2] == arr2[5] && arr2[2] == arr2[8] && arr2[2] != "") {
+    if (arr2[2] == "addCross") {
+      whoWon.innerText = "1";
+    } else {
+      whoWon.innerText = "2";
+    }
+    message.style.display = "flex";
+  } else if (arr2[1] == arr2[4] && arr2[1] == arr2[7] && arr2[1] != "") {
+    if (arr2[1] == "addCross") {
+      whoWon.innerText = "1";
+    } else {
+      whoWon.innerText = "2";
+    }
+    message.style.display = "flex";
+  } else if (arr2[0] == arr2[3] && arr2[0] == arr2[6] && arr2[0] != "") {
+    if (arr2[0] == "addCross") {
+      whoWon.innerText = "1";
+    } else {
+      whoWon.innerText = "2";
+    }
+    message.style.display = "flex";
+  } else if (arr2[0] == arr2[1] && arr2[0] == arr2[2] && arr2[0] != "") {
+    if (arr2[0] == "addCross") {
+      whoWon.innerText = "1";
+    } else {
+      whoWon.innerText = "2";
+    }
+    message.style.display = "flex";
+  } else if (arr2[3] == arr2[4] && arr2[3] == arr2[5] && arr2[3] != "") {
+    if (arr2[3] == "addCross") {
+      whoWon.innerText = "1";
+    } else {
+      whoWon.innerText = "2";
+    }
+    message.style.display = "flex";
+  } else if (arr2[6] == arr2[7] && arr2[6] == arr2[8] && arr2[6] != "") {
+    if (arr2[6] == "addCross") {
+      whoWon.innerText = "1";
+    } else {
+      whoWon.innerText = "2";
+    }
+    message.style.display = "flex";
+  } else if (arr2[0] == arr2[4] && arr2[0] == arr2[8] && arr2[0] != "") {
+    if (arr2[0] == "addCross") {
+      whoWon.innerText = "1";
+    } else {
+      whoWon.innerText = "2";
+    }
+    message.style.display = "flex";
+  } else if (arr2[2] == arr2[4] && arr2[2] == arr2[6] && arr2[2] != "") {
+    if (arr2[2] == "addCross") {
+      whoWon.innerText = "1";
+    } else {
+      whoWon.innerText = "2";
+    }
+    message.style.display = "flex";
+  } else if (arr2.indexOf("") == -1) {
+    whoWon.parentElement.innerHTML = "It's a <span>tie</span>!";
+    message.style.display = "flex";
+  }
 });
+
+const reload = document.querySelector(".message .message_container img");
+reload.addEventListener("click", function () {
+  location.reload();
+});
+document
+  .querySelector(".message .message_extras")
+  .addEventListener("click", function () {
+    location.reload();
+  });
