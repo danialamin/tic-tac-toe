0a8dd6bdfbc8069f58f2069c02d6f75625a83529
Danial Amin <danialamin489@gmail.com>
2022-08-06 19:37:27 +0500

    pl1 and pl2 have alternate turns

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const board = document.querySelector(".container .board");
 board.addEventListener("click", function (event) {
+  const boxDivs = document.querySelectorAll(".container .board div div");
+  const arr = [];
+  boxDivs.forEach(function (boxDiv) {
+    arr.push(boxDiv.className);
+  });
+
+  let crosses = 0;
+  let circles = 0;
+  for (let x of arr) {
+    if (x == "addCross") {
+      crosses += 1;
+    } else if (x == "addCircle") {
+      circles += 1;
+    }
+  }
   const boxDiv = event.target;
 
-  if (boxDiv.className === "") {
+  if (boxDiv.className === "" && crosses <= circles) {
     boxDiv.innerHTML = document.querySelector(
       ".container .mf .times"
     ).innerHTML;
     boxDiv.classList.add("addCross");
-    next();
+  } else if (boxDiv.className === "" && circles <= crosses) {
+    boxDiv.classList.add("addCircle");
   }
 });
-
-function next() {
-  board.addEventListener("click", function (event) {
-    const boxDiv = event.target;
-    if (boxDiv.className === "") {
-      boxDiv.classList.add("addCircle");
-    }
-  });
-}
